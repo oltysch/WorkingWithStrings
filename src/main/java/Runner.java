520d3a5339a5204b35cde39adf5ce316b728acaf
oltysch <oleg_tyschenko777@mail.ru>
2015-06-11 22:59:39 +0600

    Fixed error handling and JavaDocs.

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,18 +1,24 @@
 import java.io.*;
 
+import static logic.FileWorks.updateFile;
+import static logic.FileWorks.writeFile;
+
 /**
  * Created by Admin on 10.06.2015.
  */
 public class Runner {
     public static void main(String[] args) {
-
     }
 
     public void startDemonstrate() {
         //
     }
 
-    //just tests, do not use this method.
+    /**
+     * @return int (user input)
+     * Just tests, do not use this method.
+     * Requests enter the number to the correct input
+     */
     public static int testReadNumber() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int res;
--- src/main/java/logic/FileWorks.java
+++ src/main/java/logic/FileWorks.java
@@ -8,18 +8,83 @@ import java.util.List;
  */
 public class FileWorks {
 
-    public static String readFile(String fileName) throws FileNotFoundException {
-        StringBuilder sb = new StringBuilder();
+    /**
+     * @param fileName
+     * @param text
+     * It creates the file, and writes the text.
+     */
+    public static void writeFile(String fileName, String text) {
         File file = new File(fileName);
-        exists(fileName);
+
+        try {
+            if(!file.exists())
+                file.createNewFile();
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+            try {
+                out.print(text);
+            } finally {
+                out.close();
+            }
+        }catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @param fileName
+     * @param newText
+     * Adds new text in file.
+     */
+    public static void updateFile(String fileName, String newText) {
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            String oldText = readFile(fileName);
+            stringBuilder.append(oldText);
+            stringBuilder.append(newText);
+            writeFile(fileName, stringBuilder.toString());
+        }catch (FileNotFoundException e) {
+            System.err.println("Ошибка, такого файла не существует");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param fileName
+     * @param newText
+     * Adds new text in file, or works like a writeFile.
+     * this experimental method.
+     */
+    public static void updateFileExperimental(String fileName, String newText) {
+        File file = new File(fileName);
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            String oldText = readFile(fileName);
+            stringBuilder.append(oldText);
+            stringBuilder.append(newText);
+            writeFile(fileName, stringBuilder.toString());
+        }catch (FileNotFoundException e) {
+            writeFile(fileName, newText);
+        }
+    }
+
+    /**
+     * @param fileName
+     * @return String (readed text)
+     * @throws FileNotFoundException
+     */
+    public static String readFile(String fileName) throws FileNotFoundException {
+        StringBuilder stringBuilder = new StringBuilder();
+        File file = new File(fileName);
+        if (!file.exists()) throw new FileNotFoundException(fileName);
 
         try {
             BufferedReader in = new BufferedReader(new FileReader( file.getAbsoluteFile()));
             try {
                 String s;
                 while ((s = in.readLine()) != null) {
-                    sb.append(s);
-                    sb.append("\n");
+                    stringBuilder.append(s);
+                    stringBuilder.append("\n");
                 }
             } finally {
                 in.close();
@@ -28,20 +93,6 @@ public class FileWorks {
             throw new RuntimeException(e);
         }
 
-        return sb.toString();
-    }
-
-    public static void exists(String fileName) throws FileNotFoundException {
-        File file = new File(fileName);
-        if (!file.exists()) throw new FileNotFoundException(file.getName());
-    }
-
-    public static void createFile(String filename) {
-        File file = new File(filename);
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return stringBuilder.toString();
     }
 }
